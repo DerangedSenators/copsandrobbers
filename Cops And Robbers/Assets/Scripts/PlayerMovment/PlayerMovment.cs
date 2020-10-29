4b10c3845df5d99d91e79498833a2b0398a49e47
ash <ashwinjaimal@gmail.com>
2020-10-29 09:07:41 +0000

    PlayerMovement script
    Player can move on board using WSAD, no animation was tied to playermovement

--- Cops And Robbers/Assets/Scripts/PlayerMovment/PlayerMovment.cs
+++ Cops And Robbers/Assets/Scripts/PlayerMovment/PlayerMovment.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class PlayerMovment : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // Player speed variable
+    public float moveSpeed = 5f;
+    
+    // Variable for character model
+    public Rigidbody2D rb;
+
+    // Stores movement value
+    Vector2 movement;
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Input
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
     }
+
+    // Update on timer not frames
+    void FixedUpdate() {
+        //Movement
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+
+
+
 }
