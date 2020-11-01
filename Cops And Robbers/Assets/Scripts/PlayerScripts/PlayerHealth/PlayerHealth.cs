b3cc6a078ac5fcaf6583236ffa65688762ead281
mirle <190100955@aston.ac.uk>
2020-11-01 18:23:56 +0000

    Playground

---
+++ Cops And Robbers/Assets/Scripts/PlayerScripts/PlayerHealth/PlayerHealth.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/*
+*@author Peter
+*/
+
+//This script is responible for the fucntion of the healthbar and Dmg for testing
+
+
+public class PlayerHealth : MonoBehaviour
+{
+    public float maxHealth = 100f;
+    public float currentHealth;
+
+    public HealthBar healthBar;
+
+
+    //At the start of the project the player's health will equal to the max health
+    public void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+    // This fucntion checks if a user Used Space if it does the player will take 1.5 dmg
+    public void Update()
+    {
+        // Pressing the "spacebar" deals 2.3f amout of damgage
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Damage(2.3f);
+        }
+
+        //If players health reaches 0 It is removed form the scene
+        if (currentHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // This function will lower the current player health by set amout
+    public void Damage(float dmg)
+    {
+        currentHealth = currentHealth - dmg;
+
+        healthBar.SetHealth(currentHealth);
+    }
+
+
+}
+
+   
+
+    
+
---
+++ Cops And Robbers/Assets/Scripts/PlayerScripts/PlayerMovment/PlayerMovment.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovment : MonoBehaviour
+{
+
+    public float speed = 5f;
+
+    public Rigidbody2D rb;
+
+
+
+    Vector2 movment;
+
+    void Update()
+    {
+        movment.x = Input.GetAxisRaw("Horizontal");
+        movment.y = Input.GetAxisRaw("Vertical");
+    }
+
+
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movment * speed * Time.fixedDeltaTime);
+    }
+}
