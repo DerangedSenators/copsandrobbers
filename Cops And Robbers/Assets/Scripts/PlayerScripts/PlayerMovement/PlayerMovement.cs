679976b27ca60174359967f4292c92f353f27d4f
Hanzalah Ravat <hanzalah.ravat@gmail.com>
2020-11-10 18:52:35 +0000

    Remove Merge Conflict Ids from PlayerMovement.cs

--- Cops And Robbers/Assets/Scripts/PlayerScripts/PlayerMovement/PlayerMovement.cs
+++ Cops And Robbers/Assets/Scripts/PlayerScripts/PlayerMovement/PlayerMovement.cs
@@ -1,55 +1,4 @@
-<<<<<<< HEAD:Cops And Robbers/Assets/Scripts/PlayerScripts/PlayerMovement/PlayerMovement.cs
-<<<<<<< HEAD:Cops And Robbers/Assets/Scripts/PlayerMovment/PlayerMovment.cs
 using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class PlayerMovment : MonoBehaviour
-{
-    //reference the animator object within unity
-    public Animator animator;
-
-    // Player speed variable
-    public float moveSpeed = 5f;
-    
-    // Variable for character model
-    public Rigidbody2D rb;
-
-    // Stores movement value
-    Vector2 movement;
-
-    //speed of animation
-    public float runSpeed = 40f;
-
-    // Update is called once per frame
-    void Update()
-    {
-        //Input
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
-
-        //animation controls
-        animator.SetFloat("Horizontal", movement.x);
-        animator.SetFloat("Vertical", movement.y);
-        animator.SetFloat("Speed", movement.sqrMagnitude);
-
-    }
-
-    // Update on timer not frames
-    void FixedUpdate() {
-        //Movement
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
-        
-
-    }
-
-
-
-}
-=======
-=======
->>>>>>> 6d704a5c13ab7d99842521c756926d14ca545123:Cops And Robbers/Assets/Scripts/PlayerMovment/PlayerMovment.cs
-﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -105,9 +54,4 @@ namespace Me.DerangedSenators.CopsAndRobbers
             rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
         }
     }
-<<<<<<< HEAD:Cops And Robbers/Assets/Scripts/PlayerScripts/PlayerMovement/PlayerMovement.cs
 }
->>>>>>> 6d704a5 (PlayerAttack fix):Cops And Robbers/Assets/Scripts/PlayerScripts/PlayerMovement/PlayerMovement.cs
-=======
-}
->>>>>>> 6d704a5c13ab7d99842521c756926d14ca545123:Cops And Robbers/Assets/Scripts/PlayerMovment/PlayerMovment.cs
