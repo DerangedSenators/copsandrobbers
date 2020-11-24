194ebb4c7d646a2f258960ef28dd8d422f019e9d
Nisath Nasar <37841418+bissa2000@users.noreply.github.com>
2020-11-24 17:06:32 +0000

    Timer + Money Script fix (#70)
    * Money Script fix and Added GameManager
    
    MoneyUI Script was unnecessary, made MoneyManager script update the text without its need. Plus added a singleton GameManager that persists throughout the scenes.
    
    * Added Simple formatted timer
    
    * EndTimer()
    
    method to force-end timer. Used for ending if all key players of a team are dead.
    
    * Timer code cleanup

---
+++ Cops And Robbers/Assets/Scripts/LogicManager/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager gameManager;
+
+    void Awake()
+    {
+        MakeSingleton();
+    }
+
+    private void MakeSingleton() {
+        if (gameManager != null)
+        {
+            Destroy(gameObject);
+        }
+        else 
+        {
+            gameManager = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+}
---
+++ Cops And Robbers/Assets/Scripts/LogicManager/TimeManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeManager : MonoBehaviour
+{
+    private float currentTime = 0f;
+    private float startingTime = 120f;
+
+    [SerializeField] Text countdownText;
+
+    void Start()
+    {
+        currentTime = startingTime;
+    }
+
+    void Update()
+    {
+        currentTime -= 1 * Time.deltaTime;
+        string minutes = Mathf.Floor(currentTime / 60).ToString("00");
+        string seconds = Mathf.RoundToInt(currentTime % 60).ToString("00");
+        
+        countdownText.text = minutes + ":" + seconds;
+        if (currentTime <= 0) 
+        { 
+            //load roundbreak scene
+        }
+    }
+
+    /// <summary>
+    /// Call this method to force-end the timer + move to next scene. 
+    /// </summary>
+    public void EndTimer() 
+    {
+        currentTime = startingTime;
+    }
+}
