05707dfdffa5b27ab7d599c151ddad9d8cb3ce85
Hanzalah Ravat <hanzalah.ravat@gmail.com>
2020-12-11 11:51:44 +0000

    Create a camera controller

---
+++ Cops And Robbers/Assets/Scripts/PlayerScripts/CameraController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Mirror;
+using UnityEngine;
+
+namespace Me.DerangedSenators.CopsAndRobbers
+{
+    /// <summary>
+    /// Camera Controller script
+    /// </summary>
+    public class CameraController : NetworkBehaviour
+    {
+        public Camera camera;
+
+        private void Awake()
+        {
+            camera = Camera.main;
+            camera.enabled = false;
+
+        }
+
+        public override void OnStartLocalPlayer()
+        {
+            camera.enabled = true;
+        }
+    }
+}
