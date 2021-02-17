1c4df2634d3545e093f4940dc982ee8a06ee17f7
Hanzalah Ravat <hanzalah.ravat@gmail.com>
2021-02-17 18:48:27 +0000

    Make MoneyDisplay a Singleton

--- CopsAndRobbers/Assets/Scripts/MoneyPickUp/MoneyDisplay.cs
+++ CopsAndRobbers/Assets/Scripts/MoneyPickUp/MoneyDisplay.cs
@@ -1,29 +1,39 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 namespace Me.DerangedSenators.CopsAndRobbers
 {
+    /// <summary>
+    /// Script to control MoneyUI Element
+    /// </summary>
     public class MoneyDisplay : MonoBehaviour
     {
         [SerializeField] Text moneyText;
 
-        // Start is called before the first frame update
-        void Start()
+        private static MoneyDisplay _instance;
+
+        public static MoneyDisplay Instance() => _instance;
+
+        public void Awake()
         {
-
+            if (_instance != null && _instance != this)
+            {
+                Debug.Log($"Destroying instance");
+                Destroy(this.gameObject);
+            }
+            else
+            {
+                Debug.Log($"Setting instance");
+                _instance = this;
+            }
         }
-
-        // Update is called once per frame
-        void Update()
-        {
-
-        }
-
+        
         public void UpdateView(int moneyCount)
         {
             moneyText.text = "$" + moneyCount.ToString();
         }
     }
-}
+} 
