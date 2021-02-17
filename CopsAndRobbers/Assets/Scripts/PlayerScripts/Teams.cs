66b2bdbe0fefaa2baa2a02b6896e5cf99128d46c
Hanzalah Ravat <hanzalah.ravat@gmail.com>
2021-02-17 18:47:12 +0000

    Create an Enum for Teams
    Easier to read over using int values

---
+++ CopsAndRobbers/Assets/Scripts/PlayerScripts/Teams.cs
@@ -0,0 +1,12 @@
+namespace Me.DerangedSenators.CopsAndRobbers
+{
+    /// <summary>
+    /// Enum that shows the playable teams
+    /// </summary>
+    public enum Teams
+    {
+        UNASSIGNED, // 0 Used for incorporation to existing codebase.
+        COPS, // 1
+        ROBBERS // 2
+    }
+}
