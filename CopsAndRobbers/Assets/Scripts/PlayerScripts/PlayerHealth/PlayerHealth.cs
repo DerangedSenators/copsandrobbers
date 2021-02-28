0eaf4bad6633ab2926349e9a93cc81c0333afeea
Hanzalah Ravat <hanzalah.ravat@gmail.com>
2021-02-28 16:06:52 +0000

    Only Vibrate on Mobile

--- CopsAndRobbers/Assets/Scripts/PlayerScripts/PlayerHealth/PlayerHealth.cs
+++ CopsAndRobbers/Assets/Scripts/PlayerScripts/PlayerHealth/PlayerHealth.cs
@@ -54,7 +54,7 @@ namespace Me.DerangedSenators.CopsAndRobbers
         /// <param name="value">Health value, changed based on damage taken</param>
         public void SetHealth(float value)
         {
-            if(isLocalPlayer && (value < currentHealth))
+            if(isLocalPlayer && (value < currentHealth) && ControlContext.Instance.Active)
                 Vibration.Vibrate();    
             currentHealth = value;
             this.eventHealthChanged?.Invoke(currentHealth, maxHealth);
