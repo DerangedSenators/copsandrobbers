39b659148709b71d14cc6fcdb496fb680a3ee356
Hanzalah Ravat <hanzalah.ravat@gmail.com>
2021-03-07 14:23:07 +0000

    Attack Vector: Remove Helper Functions and allow it to get Manager statically
    Helper functions are now found in WeaponManager so they are unrequired here.

--- CopsAndRobbers/Assets/Scripts/PlayerScripts/PlayerAttack/AttackVector.cs
+++ CopsAndRobbers/Assets/Scripts/PlayerScripts/PlayerAttack/AttackVector.cs
@@ -1,4 +1,6 @@
+using System;
 using Mirror;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -10,12 +12,7 @@ namespace Me.DerangedSenators.CopsAndRobbers
     /// @author Hanzalah Ravat
     public abstract class AttackVector : NetworkBehaviour
     {
-        protected Vector3 mousePosition;
-        protected Vector3 mouseDir;
-        protected Vector3 attackPosition;
-        private Vector3 _offset = Vector3.up;
-
-        protected float attackOffset;
+        
         /// <summary>
         /// The LayerMask is used to detect collisions with an enemy
         /// </summary>
@@ -29,6 +26,10 @@ namespace Me.DerangedSenators.CopsAndRobbers
         protected State _state;
 
         private bool _buttonListenerSet = false;
+        /// <summary>
+        /// The Weapon Manager managing the weapon contexts
+        /// </summary>
+        public WeaponManager Manager;
 
         /// <summary>
         /// Enum object State: contains the states player can be in. Used for attack animation.
@@ -76,20 +77,13 @@ namespace Me.DerangedSenators.CopsAndRobbers
 #if UNITY_STANDALONE || UNITY_WEBPLAYER
 
         #region Client
-
-        void Update()
+        void Updatee()
         {
+            Debug.Log($"Hi There! The Local Player is {isLocalPlayer}");
             if (isLocalPlayer)
             {
-                switch (_state)
-                {
-                    case State.NORMAL:
-                        HandleAttack();
-                        break;
-                    case State.ATTACKING:
-                        HandleAttack();
-                        break;
-                }
+                Debug.Log("Running this function");
+                HandleAttack();
             }
 
         }
@@ -114,13 +108,21 @@ namespace Me.DerangedSenators.CopsAndRobbers
         }
 #endif
 
+
         /// <summary>
         /// Method that checks if the attack button is pressed on desktop and then if true, invokes the DoAttack method
         /// </summary>
-        protected virtual void HandleAttack()
+        public void HandleAttack()
         {
             if (Input.GetMouseButtonDown(0))
+            {
+                if (Manager == null)
+                {
+                    Manager = WeaponManager.LocalInstance;
+                }
+
                 DoAttack();
+            }
         }
 
         /// <summary>
@@ -132,92 +134,6 @@ namespace Me.DerangedSenators.CopsAndRobbers
         protected void CmdAttack(PlayerHealth enemy)
         {
             enemy.Damage(damageModifier);
-        }
-        
-        //--- Helper Methods ---//
-        /// <summary>
-        /// Gets the Mouse Position with Z Axis
-        /// </summary>
-        /// <param name="screenPosition"> The Current position of the player within screen-context</param>
-        /// <param name="worldCamera">The WorldView Camera</param>
-        /// <returns>A Vector3 With the relative mouse position</returns>
-        protected Vector3 GetMouseWorldPositionWithZ(Vector3 screenPosition, Camera worldCamera)
-        {
-            return Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
-
-        }
-        #if UNITY_STANDALONE || UNITY_WEBPLAYER
-        protected Vector3 GetMouseWorldPosition()
-        {
-
-            Vector3 vec = GetMouseWorldPositionWithZ(Mouse.current.position.ReadValue(), Camera.main);
-            vec.z = 0f;
-            return vec;
-        }
-        #elif UNITY_IOS || UNITY_ANDROID || UNITY_WP8 || UNITY_IPHONE
-                protected Vector3 GetMouseWorldPosition()
-                {
-                    Vector2 vec = ControlContext.Instance.AttackCircleStick.Direction;
-                    Vector3 vector3;
-                    vector3.x = vec.x;
-                    vector3.y = vec.y;
-                    vector3.z = 0f;
-                    return vec;
-                }
-        #endif
-        /// <summary>
-        /// Gets the attack point
-        /// </summary>
-        /// <returns>The attack point</returns>
-        public Vector3 GetAttackPoint()
-        {
-            return attackPosition;
-        }
-
-#if UNITY_STANDALONE || UNITY_WEBPLAYER
-        /// <summary>
-        /// Gets an attack point with a provided offset
-        /// </summary>
-        /// <param name="offset">The Offset</param>
-        /// <returns> The AttackPoint with the offset applied</returns>
-        public Vector3 GetAttackPoint(float offset)
-        {
-            return (transform.position + mouseDir * offset);
-        }
-#elif UNITY_IOS || UNITY_ANDROID || UNITY_WP8 || UNITY_IPHONE
-        public Vector3 GetAttackPoint(float offset)
-        {
-            return (transform.position + new Vector3(ControlContext.Instance.AttackCircleStick.Horizontal,
-                ControlContext.Instance.AttackCircleStick.Vertical, 0) * offset);
-        }
-#endif
-
-        /// <summary>
-        /// Return -1 if mouse is left, 1 if mouse is right or 0.
-        /// </summary>
-        /// <returns>Return -1 if mouse is left, 1 if mouse is right or 0.</returns>
-        public int MouseXPositionRelativeToPlayer()
-        {
-#if UNITY_STANDALONE || UNITY_WEBPLAYER
-            if (mousePosition.x < transform.position.x)
-            {
-                return -1;
-            }
-            else if (mousePosition.x > transform.position.x)
-            {
-                return 1;
-            }
-            return 0;
-#elif UNITY_IOS || UNITY_ANDROID || UNITY_WP8 || UNITY_IPHONE
-            if (ControlContext.Instance.AttackCircleStick.Horizontal <= 0)
-            {
-                return -1;
-            }
-            else
-            {
-                return 1;
-            }
-#endif
         }
     }
 }
