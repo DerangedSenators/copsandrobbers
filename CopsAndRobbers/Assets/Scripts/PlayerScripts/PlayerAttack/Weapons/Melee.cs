f75f94016c1269e1d5db87509475a5ec89d39b53
Hanzalah Ravat <hanzalah.ravat@gmail.com>
2021-03-07 14:23:49 +0000

    Melee: Improve Integration with AttackVector

--- CopsAndRobbers/Assets/Scripts/PlayerScripts/PlayerAttack/Weapons/Melee.cs
+++ CopsAndRobbers/Assets/Scripts/PlayerScripts/PlayerAttack/Weapons/Melee.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using UnityEngine;
 
@@ -11,8 +12,7 @@ namespace Me.DerangedSenators.CopsAndRobbers.Weapons
     {
         protected override void OnMobileAttackButtonPressed()
         {
-            SetAttackParams();
-            this._state = State.ATTACKING;
+            _state = State.ATTACKING;
             DoAttack();
         }
 
@@ -20,27 +20,14 @@ namespace Me.DerangedSenators.CopsAndRobbers.Weapons
         {
             // Do nothing for now
         }
-
-        private void SetAttackParams()
-        {
-            mousePosition = GetMouseWorldPosition(); // +new Vector3(-0.5f, -0.2f, 0);
-
-            mouseDir = (mousePosition - transform.position).normalized;
-
-            attackOffset = 0.8f;
-
-            attackPosition = (transform.position + mouseDir * attackOffset);
-        }
-
+        
         protected override void DoAttack()
         {
-            Debug.Log("Attacking");
-            Collider2D[] enemiesHit = Physics2D.OverlapCircleAll(attackPosition, attackOffset, EnemyLayer);
-
-
+            Collider2D[] enemiesHit = Physics2D.OverlapCircleAll(Manager.GetAttackPosition, Manager.attackOffset, EnemyLayer);
+            
             foreach (var enemy in enemiesHit.Select(hit => hit.GetComponent<PlayerHealth>()).Where(obj => obj != null).Where(obj => obj != this))
             {
-                base.CmdAttack(enemy);
+                CmdAttack(enemy);
             }
         }
     }
