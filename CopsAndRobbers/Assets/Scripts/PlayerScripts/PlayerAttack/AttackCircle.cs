d27fd503d64d167df06cbc3791a42468da4a46c2
Hanzalah Ravat <hanzalah.ravat@gmail.com>
2021-03-07 14:21:25 +0000

    AttackCircle: Adapt to work with weapon manager from the prefab

--- CopsAndRobbers/Assets/Scripts/PlayerScripts/PlayerAttack/AttackCircle.cs
+++ CopsAndRobbers/Assets/Scripts/PlayerScripts/PlayerAttack/AttackCircle.cs
@@ -1,25 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Me.DerangedSenators.CopsAndRobbers.Weapons;
 using UnityEngine;
 
 namespace Me.DerangedSenators.CopsAndRobbers
 {
+    /// <summary>
+    /// Class which is used to determine the position of the AttackCircle in Melee Weapons
+    /// </summary>
+    /// @authors Nisath Mohamed and Hanzalah Ravat
     public class AttackCircle : MonoBehaviour
     {
-        public PlayerAttack playerAttack;
-
-        // Start is called before the first frame update
-        void Start()
-        {
-
-        }
-
-        // Update is called once per frame
+        public WeaponManager playerAttack;
+        
         void Update()
         {
             //transform.position = playerAttack.GetAttackPoint();
             transform.position = new Vector3(playerAttack.GetAttackPoint(0.4f).x, playerAttack.GetAttackPoint(0.4f).y, 0);
-
         }
     }
 }
