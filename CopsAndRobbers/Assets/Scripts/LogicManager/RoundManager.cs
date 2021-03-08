71cba7dd4d806e277d0cd359511d0f746ed5949c
nisath <nisathesharp@Yahoo.com>
2021-03-08 20:03:14 +0000

    Move Rounds Fix

---
+++ CopsAndRobbers/Assets/Scripts/LogicManager/RoundManager.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Me.DerangedSenators.CopsAndRobbers;
+using UnityEngine;
+
+
+namespace Me.DerangedSenators.CopsAndRobbers
+{
+    public class RoundManager : MonoBehaviour
+    {
+        
+        public enum Round
+        {
+            ROUND1, ROUND2, ROUND3, ENDED
+        }
+
+        private Round _currentRound;
+
+        void Start()
+        {
+            _currentRound = Round.ROUND1;
+        }
+        
+        
+        public void LoadRound()
+        {
+            switch (_currentRound)
+            {
+                case Round.ROUND1 :
+                    TransformPlayersRound2();
+                    _currentRound = Round.ROUND2;
+                    break;
+                case Round.ROUND2 :
+                    _currentRound = Round.ROUND3;
+                    TransformPlayersRound3();
+                    break;
+                case Round.ROUND3 :
+                    _currentRound = Round.ENDED;
+                    break;
+            }
+            
+        }
+
+        public void TransformPlayersRound2()
+        {
+            if (Player.localPlayer.teamId == 1)
+            {
+                Vector3 newPos = new Vector3(186 - Player.localPlayer.playerIndex, -103, -101);
+                Player.localPlayer.transform.position = newPos;
+            }
+            else
+            {
+                Vector3 newPos = new Vector3(108 + Player.localPlayer.playerIndex, -17, -101);
+                Player.localPlayer.transform.position = newPos;
+            }
+        }
+        
+        public void TransformPlayersRound3()
+        {
+            if (Player.localPlayer.teamId == 1)
+            {
+                Vector3 newPos = new Vector3(-105 + Player.localPlayer.playerIndex, -6.6f, -30);
+                Player.localPlayer.transform.position = newPos;
+            }
+            else
+            {
+                Vector3 newPos = new Vector3(-172 + Player.localPlayer.playerIndex, -29, -30);
+                Player.localPlayer.transform.position = newPos;
+            }
+        }
+
+        public Round GetCurrentRound()
+        {
+            return _currentRound;
+        }
+
+        public void SetRound(Round targetRound)
+        {
+            _currentRound = targetRound;
+        }
+
+
+
+        void Update()
+        {
+
+        }
+    }
+}
--- CopsAndRobbers/Assets/Scripts/LogicManager/TimeManager.cs
+++ CopsAndRobbers/Assets/Scripts/LogicManager/TimeManager.cs
@@ -9,11 +9,17 @@ namespace Me.DerangedSenators.CopsAndRobbers
     public class TimeManager : MonoBehaviour
     {
         private float currentTime = 0f;
-        private float startingTime = 60f;
+        
+        private float startingTime = 10f;
+        private float round2Time = 20f;
+        
         [SerializeField] private GameObject moneyManagerGO;
 
-            [SerializeField] Text countdownText;
+        [SerializeField] Text countdownText;
 
+        [SerializeField]
+        private RoundManager _roundManager;
+        
         void Start()
         {
             currentTime = startingTime;
@@ -26,14 +32,23 @@ namespace Me.DerangedSenators.CopsAndRobbers
             string seconds = Mathf.RoundToInt(currentTime % 60).ToString("00");
 
             countdownText.text = minutes + ":" + seconds;
+            
             if (currentTime <= 0)
             {
-                //load roundbreak 
-                DontDestroyOnLoad(moneyManagerGO);
-                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+                //currentTime = startingTime;
+                currentTime = round2Time;
+                
+                _roundManager.LoadRound();
+
+                if (_roundManager.GetCurrentRound() == RoundManager.Round.ENDED)
+                {
+                    DontDestroyOnLoad(moneyManagerGO);
+                    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);    
+                }
+                
             }
         }
-
+        
         /// <summary>
         /// Call this method to force-end the timer + move to next scene. 
         /// </summary>
