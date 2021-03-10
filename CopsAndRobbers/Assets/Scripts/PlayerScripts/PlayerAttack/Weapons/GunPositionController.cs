b70aeeb582981f3448e8a68b783d4163a3769448
Hanzalah Ravat <hanzalah.ravat@gmail.com>
2021-03-10 15:22:50 +0000

    HandGun: Add Offsets to weapon

--- CopsAndRobbers/Assets/Scripts/PlayerScripts/PlayerAttack/Weapons/GunPositionController.cs
+++ CopsAndRobbers/Assets/Scripts/PlayerScripts/PlayerAttack/Weapons/GunPositionController.cs
@@ -11,12 +11,28 @@ namespace Me.DerangedSenators.CopsAndRobbers.Weapons
     {
         public Transform GunTransform;
         public WeaponManager Manager;
+        public SpriteRenderer WeaponRenderer;
         public void Update()
         {
             Vector3 mousePosition = Manager.GetMousePosition();
-            Vector3 aimPosition = (mousePosition - GunTransform.position).normalized;
+            Vector3 aimPosition = (mousePosition - Manager.ThisPlayer.transform.position).normalized;
             float angle = Mathf.Atan2(aimPosition.y, aimPosition.x) * Mathf.Rad2Deg;
             GunTransform.eulerAngles = new Vector3(0, 0, angle);
+            GunTransform.position = new Vector3(Manager.GetAttackPoint(0.75f).x, Manager.GetAttackPoint(0.4f).y, 0);
+            FlipSpriteDependingOnAxis();
+        }
+        
+        private void FlipSpriteDependingOnAxis()
+        {
+            if (Manager.MouseXPositionRelativeToPlayer() == -1)
+            {
+                WeaponRenderer.flipX = false;
+                WeaponRenderer.flipY = true;
+            }
+            else if (Manager.MouseXPositionRelativeToPlayer() == 1)
+            {
+                WeaponRenderer.flipY = false;
+            }
         }
     }
 }
