a23fa7a4c52f18a0dca3a8262cee211360ee23e8
Elliot <190223830@aston.ac.uk>
2021-03-14 13:12:16 +0000

    Documentation

--- CopsAndRobbers/Assets/Scripts/MainMenu/LoadMainMenu.cs
+++ CopsAndRobbers/Assets/Scripts/MainMenu/LoadMainMenu.cs
@@ -1,4 +1,7 @@
-﻿using UnityEngine;
+﻿///<summary>For buttons that lead to the main menu, also disables any network connection to prevent sudden attempts to reconnect.</summary>
+///@author Elliot Willis
+
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class LoadMainMenu : MonoBehaviour
