69ae4b4f0f2682c14764997a0fb457bfeb9de328
nisath <nisathesharp@Yahoo.com>
2021-03-30 17:16:46 +0100

    RoundSpawnPosition struct

--- CopsAndRobbers/Assets/Scripts/LogicManager/RoundManager.cs
+++ CopsAndRobbers/Assets/Scripts/LogicManager/RoundManager.cs
@@ -15,9 +15,11 @@
 
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using Me.DerangedSenators.CopsAndRobbers;
 using UnityEngine;
 using UnityEngine.UI;
+using Debug = UnityEngine.Debug;
 
 
 namespace Me.DerangedSenators.CopsAndRobbers
@@ -31,6 +33,9 @@ namespace Me.DerangedSenators.CopsAndRobbers
         [SerializeField] private Text currentRoundTextUI;
         private Rigidbody2D localPlayerRB;
         private int localIndex = -1;
+
+        private RoundSpawnPosition _round1SpawnPosition, _round2SpawnPosition, _round3SpawnPosition;
+        
         
         public enum Round
         {
@@ -47,6 +52,10 @@ namespace Me.DerangedSenators.CopsAndRobbers
         {
             _currentRound = Round.FREEZE;
             UpdateRoundTextView(1);
+
+            _round1SpawnPosition = new RoundSpawnPosition(new Vector2(47 + localIndex, 52), new Vector2(1 + localIndex, -56));
+            _round2SpawnPosition = new RoundSpawnPosition(new Vector2(241+localIndex,-62), new Vector2(90+localIndex,-31));
+            _round3SpawnPosition = new RoundSpawnPosition(new Vector2(-105+localIndex,-6.6f), new Vector2(-172 + localIndex, -29));
         }
         
         private void FixedUpdate() {
@@ -70,16 +79,19 @@ namespace Me.DerangedSenators.CopsAndRobbers
             switch (_currentRound)
             {
                 case Round.FREEZE :
-                    TransformPlayersRound1();
+                    TransformPlayers(1);
+                    UpdateRoundTextView(1);
                     _currentRound = Round.ROUND1;
                     break;
                 case Round.ROUND1 :
-                    TransformPlayersRound2();
+                    TransformPlayers(2);
+                    UpdateRoundTextView(2);
                     _currentRound = Round.ROUND2;
                     break;
                 case Round.ROUND2 :
+                    UpdateRoundTextView(3);
                     _currentRound = Round.ROUND3;
-                    TransformPlayersRound3();
+                    TransformPlayers(3);
                     break;
                 case Round.ROUND3 :
                     _currentRound = Round.ENDED;
@@ -94,8 +106,59 @@ namespace Me.DerangedSenators.CopsAndRobbers
             //disable player components
             
         }
+
+        private struct RoundSpawnPosition
+        {
+            public Vector2 CopPosition;
+            public Vector2 RobberPosition;
+
+            public RoundSpawnPosition(Vector2 copPosition, Vector2 robberPosition)
+            {
+                this.CopPosition = copPosition;
+                this.RobberPosition = robberPosition;
+            }
+        }
+
+        public void TransformPlayers(int roundNumber)
+        {
+            switch (roundNumber)
+            {
+              case 1:
+                  if (Player.localPlayer.teamId == 1)
+                  {
+                      localPlayerRB.position = _round1SpawnPosition.CopPosition;
+                  }
+                  else
+                  {
+                      localPlayerRB.position = _round1SpawnPosition.RobberPosition;
+                  }
+                  break;
+              case 2:
+                  if (Player.localPlayer.teamId == 1)
+                  {
+                      localPlayerRB.position = _round2SpawnPosition.CopPosition;
+                  }
+                  else
+                  {
+                      localPlayerRB.position = _round2SpawnPosition.RobberPosition;
+                  }
+                  break;
+              case 3:
+                  if (Player.localPlayer.teamId == 1)
+                  {
+                      localPlayerRB.position = _round3SpawnPosition.CopPosition;
+                  }
+                  else
+                  {
+                      localPlayerRB.position = _round3SpawnPosition.RobberPosition;
+                  }
+
+                  break;
+            }
+        }
         
         //!!!!!!!!!!!Refactor this to one code 
+        /*
         public void TransformPlayersRound1()
         {
             Debug.Log("Moving Players Round 1");
@@ -135,6 +198,7 @@ namespace Me.DerangedSenators.CopsAndRobbers
             }
             UpdateRoundTextView(3);
         }
+*/
 
         /// <summary>
         /// returns current round
