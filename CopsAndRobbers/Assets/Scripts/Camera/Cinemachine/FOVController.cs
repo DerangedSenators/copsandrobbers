79fd84bd736435146800c5c2f57ebd436f1c612c
Hanzalah Ravat <hanzalah.ravat@gmail.com>
2021-03-31 14:23:24 +0100

    Cinemachine: Initial FOV controller script and change lens to perspective

---
+++ CopsAndRobbers/Assets/Scripts/Camera/Cinemachine/FOVController.cs
@@ -0,0 +1,58 @@
+using System;
+using Cinemachine;
+using Cinemachine.Editor;
+using UnityEngine;
+
+namespace Me.DerangedSenators.CopsAndRobbers
+{
+    /// <summary>
+    /// This script is used to toggle the player FOV on button press. It will serve as a replacement for Minimap for the time being
+    /// </summary>
+    /// @author Hanzalah Ravat
+    public class FOVController: MonoBehaviour
+    {
+        public float zoomOutView;
+        public float zoomInView;
+        public CinemachineVirtualCamera CameraLens;
+        private bool active;
+        
+        public class MobileFOVController : IButtonListener
+        {
+            private readonly FOVController _controller;
+            public void onButtonPressed()
+            {
+                _controller.active = true;
+            }
+
+            public void onButtonReleased()
+            {
+                _controller.active = false;
+            }
+
+            public MobileFOVController(FOVController controller)
+            {
+                _controller = controller;
+            }
+        }
+
+        public void FixedUpdate()
+        {
+            if (active)
+            {
+                CameraLens.m_Lens.FieldOfView = zoomOutView;
+            }
+            else
+            {
+                CameraLens.m_Lens.FieldOfView = zoomInView;
+            }
+
+            if (Input.GetKeyDown(KeyCode.F) && !active)
+            {
+                active = true;
+            } else if (Input.GetKeyDown(KeyCode.F))
+            {
+                active = false;
+            }
+        }
+    }
+}
