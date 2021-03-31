2255555bf786e8bfa5a8424a1de87e4822cdcaaf
Hanzalah Ravat <hanzalah.ravat@gmail.com>
2021-03-31 13:51:42 +0100

    Add pause button handler and add method to toggle pause

--- CopsAndRobbers/Assets/Scripts/UI/UIControls.cs
+++ CopsAndRobbers/Assets/Scripts/UI/UIControls.cs
@@ -10,10 +10,40 @@ namespace Me.DerangedSenators.CopsAndRobbers
         [SerializeField] private Canvas menuCanvas;
         private bool viewMenu;
 
-        // Start is called before the first frame update
-        void Start()
+        public class PauseMenuButtonHandler : IButtonListener
         {
+            private UIControls _menu;
 
+            public PauseMenuButtonHandler(UIControls menu)
+            {
+                this._menu = menu;
+            }
+            public void onButtonPressed()
+            {
+                if (!_menu.viewMenu)
+                {
+
+                    _menu.viewMenu = true;
+
+                }
+                else
+                {
+                    _menu.viewMenu = false;
+                }
+            }
+
+            public void onButtonReleased()
+            {
+                // Do Nothing here
+            }
+        }
+
+        private void Start()
+        {
+            if (ControlContext.Instance.Active)
+            {
+                ControlContext.Instance.OptionsButton.AddListener(new PauseMenuButtonHandler(this));
+            }
         }
 
         // Update is called once per frame
@@ -29,19 +59,28 @@ namespace Me.DerangedSenators.CopsAndRobbers
                 menuCanvas.gameObject.SetActive(false);
             }
 
-            if (Input.GetKeyDown(KeyCode.Escape) && !viewMenu)
+            if (Input.GetKeyDown(KeyCode.Escape) && !menuCanvas.gameObject.activeSelf)
             {
                 viewMenu = true;
                 return;
             }
 
 
-            if (Input.GetKeyDown(KeyCode.Escape) && viewMenu)
+            if (Input.GetKeyDown(KeyCode.Escape) && menuCanvas.gameObject.activeSelf)
             {
                 viewMenu = false;
                 return;
             }
 
+        }
+
+        /// <summary>
+        /// Programatically set the visibility of the menu
+        /// </summary>
+        /// <param name="visibility"></param>
+        public void SetMenuVisibility(bool visibility)
+        {
+            viewMenu = visibility;
         }
         
     }
