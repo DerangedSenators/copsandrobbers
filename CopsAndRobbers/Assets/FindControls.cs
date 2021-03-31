ebb77271df2e2223448b6aedc34583cefe6caea2
nisath <nisathesharp@Yahoo.com>
2021-03-31 09:51:23 +0100

    Tutorial and controls

---
+++ CopsAndRobbers/Assets/FindControls.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Me.DerangedSenators.CopsAndRobbers
+{
+    /// @authors Nisath Mohamed Nasar
+    /// <summary>
+    /// Find appropriate Platform and display controls accordingly.
+    /// </summary>
+    public class FindControls : MonoBehaviour
+    {
+        public GameObject mobileControls, windowsControls;
+        
+#if UNITY_STANDALONE || UNITY_WEBPLAYER
+        private void EnableControls()
+        {
+            windowsControls.gameObject.SetActive(true);
+            mobileControls.gameObject.SetActive(false);
+        }
+#elif UNITY_IOS || UNITY_ANDROID || UNITY_WP8 || UNITY_IPHONE 
+        private void EnableControls()
+        {
+            windowsControls.gameObject.SetActive(false);
+            mobileControls.gameObject.SetActive(true);
+        }
+#endif
+
+        private void Awake()
+        {
+            EnableControls();
+        }
+    }
+}
