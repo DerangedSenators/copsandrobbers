9a5bb2ce1e2d844d903f36b154aa14ab4ee48ab1
Hanzalah Ravat <hanzalah.ravat@gmail.com>
2021-04-04 00:30:14 +0100

    Add Initial View, Layout and Theme
    This commit adds the initial Android Activity
    
    TEMPORARY: I have also mapped the Download & Install button to launching the Game Activity

--- CopsAndRobbers/platform/android/unityLibrary/src/main/java/me/derangedsenators/copsandrobbers/MainActivity.java
+++ CopsAndRobbers/platform/android/unityLibrary/src/main/java/me/derangedsenators/copsandrobbers/MainActivity.java
@@ -1,7 +1,30 @@
 package me.derangedsenators.copsandrobbers;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+import com.unity3d.player.R;
 import com.unity3d.player.UnityPlayerActivity;
 
-public class MainActivity extends UnityPlayerActivity {
+public class MainActivity extends Activity {
+
+    private Button mInstallButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.update_view);
+        super.findViewById(R.id.downloadInstallButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent gameLaunchIntent = new Intent(MainActivity.this,UnityPlayerActivity.class);
+                MainActivity.this.startActivity(gameLaunchIntent);
+            }
+        });
+    }
 
 }
